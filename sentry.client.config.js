b13625ba51fa8550a19c11b32f04a9d133994b2b
Sajid <48884791+Kurtz0420@users.noreply.github.com>
2023-03-03 15:02:50 +0500

    Update sentry.client.config.js
    added new dsn

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,5 +1,6 @@
+//old one: https://5f394c093e9d41e2a5c5ff3b2ae26a97@o1087651.ingest.sentry.io/6101188
 Sentry.init({
-  dsn: "https://5f394c093e9d41e2a5c5ff3b2ae26a97@o1087651.ingest.sentry.io/6101188",
+  dsn: "https://582f3e9db27d40679de80ce4eef539bb@o4504773889097728.ingest.sentry.io/4504773891325952",
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
